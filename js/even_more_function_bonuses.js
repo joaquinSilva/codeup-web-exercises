d4d2950be20c41e5aec63e1d1e6c9b30c2e57b19
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-10-04 11:59:06 -0500

    created several new html and js files for js function exercises.

---
+++ js/even_more_function_bonuses.js
@@ -0,0 +1,35 @@
+// (function () {
+//
+"use strict";
+
+// ================================= EVEN MORE FUNCTION BONUSES
+//
+// 1) Create a function that will return how many whitespace characters are at the beginning and end of a string.
+//
+// 2) Create a function that takes in two string inputs. If the second string input is present in the first, return the first input string with the second input string removed from it. If the second string input is present multiple times in the first, the second string will only be removed where it first occurs in the first string. If the second string input is not present in the first, return the first string as entered in the function.
+//
+// 3) Create a function that takes in a string and returns true if the last letter is an "a" (otherwise, return false).
+//
+// 4) EXTRA CHALLENGE: create a function that will return how many whitespace characters are at the beginning of a string (hint: use regex).
+//
+// 5) Create a function returnTrueMessage() that returns the string "Hey, it's true!"
+// Create a function returnFalseMessage() that returns the string "Hey, it's false!"
+// Create a function returnMessage() that takes in a function and returns the call to the function
+// Experiement passing in different functions.
+//
+// 6) Create a function, willLoginUser() that takes in a username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// })
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,8 @@
+// (function)( {
+//
+
+
+
 "use strict";
 
 /**
@@ -9,9 +14,15 @@
  * > sayHello("codeup") // returns "Hello, codeup!"
  */
 
+function sayHello(name) {
+    var message = "Hello, " + name + "!";
+    return message;
+}
+
+// ||
+//
 // function sayHello(name) {
-//     var message = ("Hello, " + name + "!");
-//     return message;
+//     return "Hello, " + name + "!";
 // }
 
 /**
@@ -59,11 +70,11 @@ var random = Math.floor((Math.random() * 3) + 1);
  * different result everytime you refresh the page if you are using the random
  * number)
  */
-
+//
 // function isTwo(input) {
 //     var output;
-//     var number = parseFloat(input);
-//     return output = (number === 2);
+//     var number = (input);
+//     return output = number === 2;
 // }
 //
 // console.log(isTwo(random));
@@ -80,11 +91,8 @@ var random = Math.floor((Math.random() * 3) + 1);
  * > calculateTip(0.15, 33.42) // returns 5.013
  */
 
-// function calculateTip(x, y) {
-//     var tipPercentage = (parseFloat(x)) / 100;
-//     var totalBill = parseFloat(y);
-//     var tipAmount = (totalBill * tipPercentage);
-//     return tipAmount;
+// function calculateTip(tipPercentage, totalBill) {
+//     return tipAmount * totalBill;
 // }
 //
 // console.log(calculateTip(10, 100));
@@ -98,17 +106,21 @@ var random = Math.floor((Math.random() * 3) + 1);
 
 
 
-function calculateTip(a, b) {
+// function calculateTip(bill, percentage) {
+//     return (bill * percentage);
+// }
+//
+// var clientBill = prompt("Please enter the total amount of your bill.");
+// var tipPercentage = prompt("Please enter the percentage you would like to tip.");
+//
+// alert("Your tip amount is $" + calculateTip(clientBill, tipPercentage));
 
-    var x = prompt("Please enter the percentage you would like to tip.");
-    var y = prompt("Please enter the total amount of your bill.");
-    var tipPercentage = (parseFloat(x))/100;
-    var totalBill = parseFloat(y);
-    var tipAmount = (totalBill * tipPercentage);
-    return tipAmount.toFixed(2);
-}
+//
+//
+// function alertTipMessage(tip, numberPlaces) {
+//     alert("Your tip amount is $" + tip.toFixed(2));
+// }
 
-alert("Your tip amount is $" + calculateTip());
 
 /**
  * TODO:
@@ -125,15 +137,11 @@ alert("Your tip amount is $" + calculateTip());
  * > applyDiscount(45.99, 0.12) // 40.4712
  */
 
-// function applyDiscount(x, y) {
-//     var originalPrice = parseFloat(x);
-//     var discountPercent = (parseFloat(y));
-//     var newPrice = (originalPrice - (originalPrice * discountPercent));
-//     return newPrice;
+// function applyDiscount(originalPrice, discountPercent) {
+//     return originalPrice - (originalPrice * discountPercent);
 // }
-//
 //     console.log(applyDiscount(100, .1));
-
+//
 //
 
 
@@ -141,115 +149,3 @@ alert("Your tip amount is $" + calculateTip());
 
 
 
-/** Function Drills
-
-
-// Functions using conditionals but not loops or arrays:
-*/
-
-//     Make a function named isOdd(number)
-//
-// function isOdd(number) {
-//     var response = (parseFloat(number) % 2) !== 0;
-//     return response;
-// }
-//
-// console.log(isOdd(0));
-
-// Make a function named isEven(number)
-//
-// function isEven(number) {
-//     var response = (parseFloat(number) % 2) === 0;
-//     return response;
-// }
-//
-// console.log(isEven(3));
-
-// Make a function named identity(input) that returns the input exactly as provided.
-
-// function identity(input) {
-//     var output = input;
-//     return output;
-// }
-//
-// console.log(identity("hello"));
-
-// Make a function named isFive(input)
-
-// function isFive(input) {
-//     var output = (parseFloat(input) === 5);
-//     return output;
-// }
-//
-// console.log(isFive(5));
-
-// Make a function named addFive(input) that adds five to some input.
-
-// function addFive(input) {
-//     var output = parseFloat(input) + 5;
-//     return output;
-// }
-//
-// console.log(addFive(0));
-
-// Make a function named isMultipleOfFive(input)
-
-// function isMultipleOfFive(input) {
-//     var output = (parseFloat(input) % 5) === 0;
-//     return output;
-// }
-//
-// console.log(isMultipleOfFive(55));
-//
-
-// Make a function named isThree(input)
-
-// function isThree(input) {
-//     var output = (input % 3) === 0
-//     return output;
-// }
-//
-// console.log(isThree(9));
-
-// Make a function named isMultipleOfThree(input)
-// Make a function named isMultipleOfThreeAndFive(input)
-// Make a function named isMultipleOf(target, n) which checks if target is evenly divisible by n
-// Make a function named isTrue(boolean)
-// Make a function named isFalse(boolean)
-// Make a function named isTruthy(input), remember that values other than true will behave like true
-// Make a function named isFalsy(input), remember that values other than false behave like false
-// Make a function named isVowel(letter)
-// Make a function named isConsonant(letter)
-// Make a function named isCapital(letter)
-// Make a function named isLowerCase(letter)
-// Make a function named hasLowerCase(string) that returns if a string has any lower cased letter
-// Make a function named isSpace(letter) that returns if a character is a space character
-// Make a function named isZero(number)
-// Make a function named notZero(input) that returns true if the input is not zero
-// Write a function named lowerCase(string)
-// Write a function named double(n) that returns a number times two
-// Write a function named triple(n) that returns a number times 3
-// Write a function named quadruple(n) that returns a number times 4
-// Write a function named half(n) that returns 1/2 of the provided input
-// Write a function named subtract(a, b) that returns a minus b
-// Write a function named multiply(a, b) that returns the product of a times b
-// Write a function named divide(a, b) that returns a divided by b
-// Write a function named remainder(a, b) that returns the remainder after dividing a by b
-// Make a function named modulo(a, b) that returns the returns the remainder after dividing a by b
-// Write a function named cube(n) that returns n * n * n
-// Write a function named squareRoot(n) that returns the square root of the input
-// Write a function named cubeRoot(n) that returns the cube root of the input
-// Write a function named invertSign(number) that returns a negative version of a postive number, a positve version of negative, and false for all else.
-// Write a function named degreesToRadians(number)
-// Write a function named radiansToDegrees(number)
-// Make a function named isBlank(input) that determines if a given input is spaces, newline characters, or tabs.
-// Make a function named trim(string) that removes empty spaces before and after the input.
-// Make a function named areEqual(input1, input2) that returns if both inputs have the same value
-// Make a function named areIdentical(input1, input2) that returns if both inputs are same value and data type.
-// Make a function named not(input) returns the input with a flipped boolean
-// Make a function named notNot(input) that the negation of the negation of the input.
-// Make a function named and(predicate1, predicate2) that returns the logical operation of AND
-// Make a function named or(predicate1, predicate2) that returns the logical operation of OR
-// Write a function called reverseString(string) that reverses a string
-// Make a function named absoluteValue(number) that returns the absolute value of a number.
-// Make a function named rollDice(sides) that takes in an argument containing the number of sides the die should have. Generate a random number between 1 up to and including the number of sides.
