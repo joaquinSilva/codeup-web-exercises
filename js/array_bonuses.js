f36c71a3eb97d54da233f25d13e72d9a8af0b836
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-10-10 16:44:07 -0500

    created html file for js array bonuses and started the array bonus.

--- js/array_bonuses.js
+++ js/array_bonuses.js
@@ -1,23 +1,31 @@
-================================= ARRAY BONUSES
+// ================================= ARRAY BONUSES
+//
+// 1. Create a function that returns a random day of the week
 
-1. Create a function that returns a random day of the week
+var randomNumber = Math.floor((Math.random() * 7) + 1);
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function randomDay(daysOfTheWeek) {
+    return daysOfTheWeek[randomNumber - 1];
+}
 
-2. Create a function that takes a single letter and returns what number the letter is in the alphabet. Ignore case.
-
-someFunction("a") // returns 1
-someFunction("z") // returns 26
-
-3. Create a function that returns the longest string in a given array of string elements
-
-4. Create a function that takes in two arrays of number inputs. If all numbers added together in the first array is equal to all the number inputs added in the second array, return true, otherwise, false. Only add together numeric elements but either array may contain non-numeric elements.
-
-    var arr1 = ['bob', 1, true, 1, 2];
-var arr2 = [2, null, undefined, 0, 2, "apple"]
-exampleFunction(arr1, arr2) // returns true
-
-
-
-
+console.log(randomDay(randomNumber));
+//
+// 2. Create a function that takes a single letter and returns what number the letter is in the alphabet. Ignore case.
+//
+// someFunction("a") // returns 1
+// someFunction("z") // returns 26
+//
+// 3. Create a function that returns the longest string in a given array of string elements
+//
+// 4. Create a function that takes in two arrays of number inputs. If all numbers added together in the first array is equal to all the number inputs added in the second array, return true, otherwise, false. Only add together numeric elements but either array may contain non-numeric elements.
+//
+//     var arr1 = ['bob', 1, true, 1, 2];
+// var arr2 = [2, null, undefined, 0, 2, "apple"]
+// exampleFunction(arr1, arr2) // returns true
+//
+//
+//
+//
 
 
 
